2cce82b81d8f577d6ca811bf0a4acf614571cb35
Naimullah Md Jakaria <jakaria64.nm@hotmail.com>
2025-09-02 20:22:46 +0600

    2.0

--- includes/config.php
+++ includes/config.php
@@ -1,5 +1,5 @@
 <?php
 // ehr_app/includes/config.php
-define('APP_URL', 'http://localhost/ehr_app');
+define('APP_URL', 'https://localhost/ehr_app');
 // You might add other global configs here later
 ?>
